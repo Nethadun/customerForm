016da2098356984edece975cd4aec185b5f8c0da
chathurab <chathurab@openqubit.com>
2019-06-17 12:30:02 +0530

    first comment

---
+++ customerController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: 6/17/2019
+ * Time: 9:37 AM
+ */
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Chathura
+ * Date: 6/17/2019
+ * Time: 9:37 AM
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<form action="customerController.php" method="post" id="nameform">
+    <table>
+        <tr>
+            Customer ID:
+        </tr>
+
+    </table>
+<!--    Customer ID: <input type="text" name="cid"></br>-->
+<!--    Customer Name: <input type="text" name="cName"></br>-->
+<!--    Address: <input type="text" name="address"></br>-->
+<!--    Mobile Number: <input type="text" name="mob"></br>-->
+<!--    <button type="submit" form="nameform" value="submit">Submit</button>-->
+<!--    <button type="reset" value="Reset">Reset</button>-->
+</form>
+</body>
+</html>
+
